f13ef77b097b65efc99e111cb5b8397bedd547ae
nils <test@test.com>
2024-01-15 12:48:25 +0100

    added functionality for stepLength activity

--- app/src/main/java/uek335/project/stepcounter/StepLengthActivity.java
+++ app/src/main/java/uek335/project/stepcounter/StepLengthActivity.java
@@ -2,13 +2,40 @@ package uek335.project.stepcounter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class StepLengthActivity extends AppCompatActivity {
+
+    private View buttonSubmit;
+    private TextView valueLength;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_length);
+
+        valueLength = findViewById(R.id.valueLenght);
+        buttonSubmit = findViewById(R.id.submitButton);
+        sharedPreferences = getSharedPreferences("MyPrefsStepLength", MODE_PRIVATE);
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stepLength = valueLength.getText().toString();
+
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("lenght", stepLength);
+                editor.apply();
+
+                Intent intent = new Intent(StepLengthActivity.this, LeaderBoardActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
